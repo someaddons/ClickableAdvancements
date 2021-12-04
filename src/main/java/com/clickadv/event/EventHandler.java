73cc420ae7d6d29b34dc8c16a396184a8bfe36fc
someaddons <someaddons@github.com>
2021-12-04 13:05:00 +0100

    1.18 port

--- src/main/java/com/clickadv/event/EventHandler.java
+++ src/main/java/com/clickadv/event/EventHandler.java
@@ -22,7 +22,9 @@ public class EventHandler
         // Inverted condition of #PlayerAdvancements to exclude when we already sent it to all
         if (!(event.getAdvancement().getDisplay().shouldAnnounceChat() && event.getPlayer().level.getGameRules().getBoolean(GameRules.RULE_ANNOUNCE_ADVANCEMENTS)))
         {
-            if (ClickAdvancements.config.getCommonConfig().showAllInLocalChat.get() || event.getAdvancement().getDisplay().shouldAnnounceChat())
+            if ((ClickAdvancements.config.getCommonConfig().showAllInLocalChat.get() && event.getAdvancement().getParent() != null) || event.getAdvancement()
+              .getDisplay()
+              .shouldAnnounceChat())
             {
                 event.getPlayer().sendMessage(AdvancementHelper.buildAdvancementChatInfo(event.getAdvancement()), event.getPlayer().getUUID());
             }
