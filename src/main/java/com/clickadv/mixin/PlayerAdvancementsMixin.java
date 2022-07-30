79cf14e54c31770227b715ae6f707c753392e413
someaddons <someaddons@github.com>
2022-07-30 21:54:31 +0200

    v2.8: 1.19 port

--- src/main/java/com/clickadv/mixin/PlayerAdvancementsMixin.java
+++ src/main/java/com/clickadv/mixin/PlayerAdvancementsMixin.java
@@ -4,7 +4,6 @@ import com.clickadv.ClickAdvancements;
 import com.clickadv.advancements.AdvancementHelper;
 import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.PlayerAdvancementTracker;
-import net.minecraft.network.message.MessageType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.world.GameRules;
@@ -36,7 +35,7 @@ public class PlayerAdvancementsMixin
               .getDisplay()
               .shouldAnnounceToChat())
             {
-                owner.sendMessage(AdvancementHelper.buildAdvancementChatInfo(advancement), MessageType.CHAT);
+                owner.sendMessage(AdvancementHelper.buildAdvancementChatInfo(advancement), false);
             }
         }
     }
