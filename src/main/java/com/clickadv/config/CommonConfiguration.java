f80c3d6347907bdc70bf58461fabaec864abd2f7
someaddons <someaddons@github.com>
2023-07-25 11:45:56 +0200

    3.1: New config format, added cupboard dep

--- src/main/java/com/clickadv/config/CommonConfiguration.java
+++ src/main/java/com/clickadv/config/CommonConfiguration.java
@@ -1,24 +1,35 @@
 package com.clickadv.config;
 
-import net.minecraftforge.common.ForgeConfigSpec;
+import com.cupboard.config.ICommonConfig;
+import com.google.gson.JsonObject;
 
-public class CommonConfiguration
+public class CommonConfiguration implements ICommonConfig
 {
-    public final ForgeConfigSpec                      ForgeConfigSpecBuilder;
-    public final ForgeConfigSpec.ConfigValue<Boolean> showAllInLocalChat;
+    public boolean showAllInLocalChat = true;
 
-    protected CommonConfiguration(final ForgeConfigSpec.Builder builder)
+    public CommonConfiguration()
     {
-        builder.push("Config category");
 
-        builder.comment("By default vanilla only displays advancements in chat which get broadcasted to all online players,"
-                          + "this setting turns on showing all advancements achieved in chat instead. That way you can find"
-                          + "the advancements you got easier/see better which you get. Some of these may have no actual text though because"
-                          + " the advancement itself doesnt have any, e.g. Categories are advancements too. default = true");
-        showAllInLocalChat = builder.define("showAllInLocalChat", true);
+    }
 
-        // Escapes the current category level
-        builder.pop();
-        ForgeConfigSpecBuilder = builder.build();
+    @Override
+    public JsonObject serialize()
+    {
+        final JsonObject root = new JsonObject();
+
+        final JsonObject entry = new JsonObject();
+        entry.addProperty("desc:", " By default vanilla only displays advancements in chat which get broadcasted to all online players."
+                                     + "Enabling this setting shows all advancements earned in chat."
+                                     + "Some of these may be lacking text when the advancement itself does not have any, e.g. Categories are advancements too. default: true");
+        entry.addProperty("showAllInLocalChat", showAllInLocalChat);
+        root.add("showAllInLocalChat", entry);
+
+        return root;
+    }
+
+    @Override
+    public void deserialize(JsonObject data)
+    {
+        showAllInLocalChat = data.get("showAllInLocalChat").getAsJsonObject().get("showAllInLocalChat").getAsBoolean();
     }
 }
