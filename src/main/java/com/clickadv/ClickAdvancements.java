afac98c30f9302fd708c0766b75dae6db7793221
someaddons <someaddons@github.com>
2023-07-25 12:41:05 +0200

    3.1: added cupboard config

--- src/main/java/com/clickadv/ClickAdvancements.java
+++ src/main/java/com/clickadv/ClickAdvancements.java
@@ -1,6 +1,7 @@
 package com.clickadv;
 
-import com.clickadv.config.Configuration;
+import com.clickadv.config.CommonConfiguration;
+import com.cupboard.config.CupboardConfig;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,15 +11,13 @@ import java.util.Random;
 // The value here should match an entry in the META-INF/mods.toml file
 public class ClickAdvancements implements ModInitializer
 {
-    public static final String        MODID  = "clickadv";
-    public static final Logger        LOGGER = LogManager.getLogger();
-    public static       Configuration config = new Configuration();
-    public static       Random        rand   = new Random();
+    public static final String                              MODID  = "clickadv";
+    public static final Logger                              LOGGER = LogManager.getLogger();
+    public static       CupboardConfig<CommonConfiguration> config = new CupboardConfig<>(MODID, new CommonConfiguration());
+    public static       Random                              rand   = new Random();
 
     @Override
     public void onInitialize()
     {
-        config.load();
-        LOGGER.info(MODID + " mod initialized");
     }
 }
